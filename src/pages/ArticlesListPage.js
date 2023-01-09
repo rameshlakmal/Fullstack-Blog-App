e918402ec3873f8a548de52829b9b5778905774a
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-09 20:55:07 +0530

    Creating and linking the articles list

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,6 +1,20 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 const ArticleListPage = ()=>{
     return(
-        <h1>This is the article list page</h1>
+        <>
+       <h1>Articles</h1>
+        {articles.map(article =>(
+            <Link key={article.name} className="article-list-item" to = {`/articles/${article.name}`}>
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0.150)}...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button>See More</button>
+                </p>
+            </Link>
+            
+        ))}
+        </>
     )
 }
 
