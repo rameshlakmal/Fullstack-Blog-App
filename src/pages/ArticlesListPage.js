60d5af530cf72dae2497378b00a29e558e4244f1
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-09 21:38:29 +0530

    Making article list modular

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,19 +1,11 @@
 import articles from "./article-content";
-import { Link } from "react-router-dom";
+import ArticleList from "../components/ArticlesList";
 
 const ArticleListPage = ()=>{
     return(
         <>
        <h1>Articles</h1>
-        {articles.map(article =>(
-            <Link key={article.name} className="article-list-item" to = {`/articles/${article.name}`}>
-                <h3>{article.title}</h3>
-                <p>{article.content[0].substring(0.150)}...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button>See More</button>
-                </p>
-            </Link>
-            
-        ))}
+            <ArticleList articles={articles}/>
         </>
     )
 }
