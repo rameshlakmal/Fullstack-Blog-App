13a4053b6a44f14d1a5b4d955ebd131ba40bdb1e
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-09 23:47:02 +0530

    Upvoting articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,11 +1,34 @@
 import express from 'express';
 
-const app = express();
+let articleInfo =[{
+    name: 'learn-react',
+    upvotes: 0,
+},{
+    name: 'learn-node',
+    upvotes: 0,
+},{
+    name: 'mongodb',
+    upvotes: 0,
+}]
 
-app.get('/hello', (req,res)=>{
-    res.send('Hello');
-})
+const app = express();
+app.use(express.json());
+
+
+app.put('/api/articles/:name/upvote',(req,res)=>{
+    const {name} = req.params;
+    const article =articleInfo.find(a => a.name === name);
+
+    if(article){
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes}`);
+    }else{
+        res.send('That article doesn\'t exist');
+    }
+});
+
+
 
 app.listen(8000,()=>{
-    console.log('Server is listenining on port 8000 ')
+    console.log('Server is listening on port 8000 ')
 })
