c6d608d5863cd34a7e82ad5718767e987347a639
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-10 12:47:38 +0530

    rewrite upvote endpoint

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -22,12 +22,21 @@ app.get('/api/articles/:name',async (req,res)=>{
 });
 
 
-app.put('/api/articles/:name/upvote',(req,res)=>{
+app.put('/api/articles/:name/upvote', async(req,res)=>{
     const {name} = req.params;
-    const article =articleInfo.find(a => a.name === name);
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+    await db.collection('articles').updateOne({name},{
+        $inc: {upvotes: 1},
+    });
+
+    const article = await db.collection('articles').findOne({name});
+
 
     if(article){
-        article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes}`);
     }else{
         res.send('That article doesn\'t exist');
