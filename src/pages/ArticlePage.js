d77378451a8bcaf01fd35f3696f7e69eb9a984bc
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-10 19:47:31 +0530

    Adding data to page component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,27 @@
+import { useState, useEffect} from "react";
+import axios from 'axios';
 import { useParams } from "react-router-dom";
 import articles from './article-content';
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = ()=>{
-    const {articleId} = useParams();
+const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments:[]});
+const {articleId} = useParams();
+
+useEffect(()=>{
+    const loadArticleInfo = async() =>{
+        const response = await axios.get(`/api/articles/${articleId}`);
+        const newArticleInfo = response.data;
+        setArticleInfo (newArticleInfo);
+    }
+    loadArticleInfo();
+
+}, []);
+
+
     const article = articles.find(article => article.name === articleId);
+
+
 
     if(!article){
         return <NotFoundPage/>
@@ -13,6 +30,7 @@ const ArticlePage = ()=>{
     return(
         <>
         <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
         {article.content.map((paragraph, i) =>(
             <p key={i}>{paragraph}</p>
         ))}
