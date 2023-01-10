bb4a8900b78eda5d24a38803cd908d6d400b17ba
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-10 13:09:23 +0530

    rewrite comments endpoint

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {MongoClient} from 'mongodb';
+import {db,dbconnection} from './db.js';
 
 
 const app = express();
@@ -7,11 +8,6 @@ app.use(express.json());
 
 app.get('/api/articles/:name',async (req,res)=>{
     const {name} = req.params;
-
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
 
     const article = await db.collection('articles').findOne({name});
     if(article){
@@ -25,10 +21,6 @@ app.get('/api/articles/:name',async (req,res)=>{
 app.put('/api/articles/:name/upvote', async(req,res)=>{
     const {name} = req.params;
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
     await db.collection('articles').updateOne({name},{
         $inc: {upvotes: 1},
     });
@@ -45,15 +37,17 @@ app.put('/api/articles/:name/upvote', async(req,res)=>{
 
 
 
-app.post('/api/articles/:name/comments',(req,res)=>{
+app.post('/api/articles/:name/comments', async (req,res)=>{
     const {name} =req.params;
     const {postedBy,text} = req.body;
 
+    await db.collection('articles').updateOne({name},{
+        $push: {comments: {postedBy, text}},
+    });
 
-    const article =articleInfo.find(a => a.name === name);
+    const article = await db.collection('articles').findOne({name});
 
     if(article){
-        article.comments.push({postedBy,text});
         res.send(article.comments);
     }else{
         res.send('That article doesn\'t exist');
@@ -62,7 +56,9 @@ app.post('/api/articles/:name/comments',(req,res)=>{
 });
 
 
-
-app.listen(8000,()=>{
-    console.log('Server is listening on port 8000 ')
+dbconnection(()=>{
+    console.log('Successfully connected to the database...!')
+    app.listen(8000,()=>{
+        console.log('Server is listening on port 8000 ')
+    });
 })
