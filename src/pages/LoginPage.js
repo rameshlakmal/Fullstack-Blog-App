a3821a5d404564395f994dc03722b8a30f8acb4f
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-11 01:03:39 +0530

    Create login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,43 @@
+import {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {getAuth,signInWithEmailAndPassword} from 'firebase/auth';
+
 const LoginPage = ()=>{
+    const [email,sendEmail]= useState('');
+    const [password, setPassword] = useState('');
+    const [error,setError] = useState('');
+
+    const navigate = useNavigate();
+
+
+    const logIn = async()=>{
+        try{
+            await signInWithEmailAndPassword(getAuth(),email,password);
+            navigate('/articles');
+        }catch(e){
+            setError(e.message);
+        }
+    }
+
+
     return(
+        <>
         <h1>Login</h1>
+        {error && <p className="error">{error}</p>}
+        <input
+            placeholder="Your email address"
+            value={email}
+            onChange={e=> sendEmail(e.target.value)}/>
+
+        <input
+            type="password"
+            placeholder="Your Password"
+            value={password}
+            onChange={e=>setPassword(e.target.value)}/>
+
+        <button onClick={logIn}>Login</button>
+        <Link to="/create-account">Don't have account..? Create one here...</Link>
+        </>
     );
 }
 
